136387cb09193e4e415c0f4e645de091a669ee6d
Ivan Reinaldo <reinaldoivan777@gmail.com>
2019-08-15 10:03:39 +0700

    apply prettier

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
-import models from './models'
-import routes from './routes'
+import models from './models';
+import routes from './routes';
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     req.context = {
         models,
         me: models.users[1]
     };
-    next()
-})
+    next();
+});
 
-app.use('/session', routes.session)
-app.use('/users', routes.user)
-app.use('/messages', routes.message)
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
 app.listen(process.env.PORT, () => {
-    console.log(`This app is running on port ${process.env.PORT}`)
-})
+    console.log(`This app is running on port ${process.env.PORT}`);
+});
